01f75df271bad20a007c3f7d42495f18c8b78637
liangxiaoming <liangxiaoming@yy.com>
2020-11-04 12:53:47 +0800

    feat: init project

---
+++ projects/dynamic-system-host/README.md
@@ -0,0 +1,14 @@
+# Dynamic System Host Example
+
+This example demos a basic host application loading remote component.
+
+- `app1` is the host application.
+- `app2` standalone application which exposes `Widget` component.
+
+
+# Running Demo
+
+Run `yarn start`. This will build and serve both `app1`, `app2` on ports `3001`, `3002` respectively.
+
+- [localhost:3001](http://localhost:3001/) (HOST)
+- [localhost:3002](http://localhost:3002/) (STANDALONE REMOTE)
---
+++ projects/dynamic-system-host/demo1/src/@emp-demo2.d.ts
@@ -0,0 +1,20 @@
+declare module '@emp/demo2/bootstrap' {
+  export {}
+}
+declare module '@emp/demo2/components/Hello' {
+  /// <reference types="react" />
+  export interface HelloProps {
+    compiler: string
+    framework: string
+  }
+  const Hello: (props: HelloProps) => JSX.Element
+  export default Hello
+}
+declare module '@emp/demo2/helper' {
+  export const log: (d: any) => void
+}
+declare module '@emp/demo2/index' {}
+declare module '@emp/demo2' {
+  import main = require('@emp/demo2/index')
+  export = main
+}
