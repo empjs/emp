2355c9606e7e520d19c482d5254a3c127173be72
doerme <398921432@qq.com>
2020-11-04 12:53:47 +0800

    feat: init project

---
+++ projects/dynamic-system-host/demo1/src/bootstrap.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import Hello from 'src/components/Hello'
+import {log} from '@emp/demo2/helper'
+import Hello2 from '@emp/demo2/components/Hello'
+log('==============testing!!!!==============================')
+ReactDOM.render(
+  <>
+    <Hello />
+    <div style={{backgroundColor: '#eee', padding: '20px'}}>
+      <h2>Demo2 Component: Hello!!!!!</h2>
+      <Hello2 compiler={'emp'} framework={'react'} />
+    </div>
+  </>,
+  document.getElementById('emp-root'),
+)
