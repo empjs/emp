ad442114ffe1a11cbd273f6b3859e10a3c59e74e
flyyuan <740004544@qq.com>
2020-11-06 14:53:56 +0800

    feat: 增加 Vue2 demo

---
+++ projects/vue2/src/components/Content.vue
@@ -0,0 +1,13 @@
+<template>
+  <div style="color: red">{{ title }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Hello Vue2",
+    };
+  },
+};
+</script>
---
+++ projects/vue2/src/main.js
@@ -0,0 +1,8 @@
+import Vue from "vue"
+import App from "./App"
+
+Vue.config.productionTip = false
+
+new Vue({
+    render:h=>h(App)
+}).$mount('#emp-root')
