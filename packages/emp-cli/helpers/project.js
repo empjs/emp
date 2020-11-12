4c5bdd550cfecebf6bca965fada7b551cb7cb17f
xuhongbin <xuhongbin@yy.com>
2020-11-12 14:38:53 +0800

    chore: 升级webpack 5.4.0

--- packages/emp-cli/helpers/project.js
+++ packages/emp-cli/helpers/project.js
@@ -30,7 +30,7 @@ module.exports = {
       ) {
         withReact(remoteConfigFn)({config, env, empEnv, hot, webpack})
       } else {
-        console.log('======== remoteConfigFn =========')
+        // console.log('======== remoteConfigFn =========')
         remoteConfigFn({config, env, empEnv, hot, webpack})
       }
     } else {
