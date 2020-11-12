d3792c9c950b8bc4a67bc153ab1da359220d392f
kuangjianlong <kuangjianlong@yy.com>
2020-11-12 12:45:03 +0800

    feat: 更新babel

--- packages/emp-frameworks/preact/index.js
+++ packages/emp-frameworks/preact/index.js
@@ -1,35 +1,35 @@
 const path = require('path')
 
 module.exports = fn => ec => {
-  const {config} = ec
+  const { config } = ec
   const srcPath = path.resolve('./src')
   config.entry('index').clear().add(path.join(srcPath, 'index.js'))
   config.module
-    .rule('preact')
-    .test(/\.(js|jsx|ts|tsx)$/)
-    .exclude
-    .add(/node_modules/)
-    .end()
+    .rule('scripts')
     .use('babel')
-    .loader('babel-loader')
-    .options({
-      plugins: [
-      [
-        require('@babel/plugin-transform-react-jsx').default,
-        {
-          pragma: "h",
-          pragmaFrag: "Fragment"
-        }
-      ],
-      [
-        require.resolve('babel-plugin-jsx-pragmatic'),
-        {
-          module: "preact",
-          import: "h",
-          export: "h"
-        }
-      ]
-    ]
+    .tap(args => {
+      args = {
+        ...args,
+        plugins: [
+          [
+            require('@babel/plugin-transform-react-jsx').default,
+            {
+              pragma: "h",
+              pragmaFrag: "Fragment"
+            }
+          ],
+          [
+            require.resolve('babel-plugin-jsx-pragmatic'),
+            {
+              module: "preact",
+              import: "h",
+              export: "h"
+            }
+          ]
+        ]
+      }
+      console.log(args)
+      return args
     })
   return fn(ec)
 }
