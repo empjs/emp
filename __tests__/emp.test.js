04a0a82fb8b6708540c15cb25f58c6ef46ab45d0
xuhongbin <xuhongbin@yy.com>
2020-11-12 19:39:05 +0800

    test: 更新用例样式

--- __tests__/emp.test.js
+++
@@ -1,9 +0,0 @@
-const {cli} = require('./helper')
-
-describe('@efox/emp-cli', () => {
-  test('emp-cli command return 0', async () => {
-    let result = await cli('emp -h', './packages/emp-cli')
-    // console.log('result', result)
-    expect(result.code).toBe(0)
-  })
-})
