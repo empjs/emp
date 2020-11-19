9c3f34d1ead76fb7a3da51b43825cce80402314f
xuhongbin <xuhongbin@yy.com>
2020-11-20 02:05:56 +0800

    chore(add): 增加 element plus demo

---
+++ projects/element-plus/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <img class="logo" alt="Vue logo" src="./logo.png">
+  <HelloWorld :msg="msg"/>
+  <div class="btn">
+    <el-button type="primary" @click="startHacking">
+      Get Started
+    </el-button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    const msg = ref("Welcome to Element Plus, a Vue 3.0 based component library")
+    const startHacking = () => {
+      msg.value = "Start coding with Element Plus with 💖"
+    }
+    return {
+      msg,
+      startHacking
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.logo {
+  width: 50%;
+}
+.btn {
+  margin-top: 100px;
+}
+</style>
---
+++ projects/element-plus/src/bootstrap.js
@@ -0,0 +1,6 @@
+import ElementPlus from 'element-plus'
+import 'element-plus/lib/theme-chalk/index.css'
+import {createApp} from 'vue'
+import App from './App.vue'
+
+createApp(App).use(ElementPlus).mount('#emp-root')
