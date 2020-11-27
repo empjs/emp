24046b2971d7bc324547c4608a43b7e320626024
xuhongbin <xuhongbin@yy.com>
2020-11-27 18:14:23 +0800

    chore: 优化esbuild

--- packages/emp-compile/esbuild/index.js
+++ packages/emp-compile/esbuild/index.js
@@ -3,15 +3,11 @@ const {ESBuildPlugin, ESBuildMinifyPlugin} = require('esbuild-loader')
 module.exports = fn => ec => {
   const {config, env, hot} = ec
   const isDev = env === 'development'
-  config.module
-    .rule('scripts')
-    .use('babel')
-    .loader('esbuild-loader')
-    .options({
-      target: 'esnext',
-      loader: 'tsx',
-      plugin: ['babel-plugin-named-asset-import'],
-    })
+  config.module.rule('scripts').use('babel').loader('esbuild-loader').options({
+    target: 'esnext',
+    loader: 'tsx',
+    // plugin: ['babel-plugin-named-asset-import'],
+  })
   config.plugin('esbuild-plugin').use(ESBuildPlugin)
   config.optimization.minimizer('esbuild-mini').use(ESBuildMinifyPlugin)
   if (hot && isDev) {
