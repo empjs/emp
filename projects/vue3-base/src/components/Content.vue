2aa0ba437ed4e93e9b034c96e9b9a68ff546a168
xuhongbin <xuhongbin@yy.com>
2020-12-03 21:04:10 +0800

    chore(vue3): 验证vue3部分不编译的问题

--- projects/vue3-base/src/components/Content.vue
+++ projects/vue3-base/src/components/Content.vue
@@ -1,16 +1,26 @@
 <template>
 <div>
   <h2>{{ title }}</h2>
+  <p>=== Button Components begin: ===</p>
+  <emp-button />
+    <p>=== Button Components end: ===</p>
 </div>
 </template>
 
 <script>
+import {  defineAsyncComponent } from 'vue'
+//import Button from './Button'
 export default {
   name: 'Content',
+  components: {
+    'emp-button':defineAsyncComponent(() =>
+      import('./Button.vue')
+    ),
+  },
   setup() {},
   data() {
     return {
-      title: "Vue 3 BASE Component in Action..",
+      title: "Vue 3 BASE Component Content",
     };
   },
 };
