0c10bb6b3254afb622b804ffdbd439208fcf40f7
xuhongbin <xuhongbin@yy.com>
2020-12-04 18:02:10 +0800

    docs: 增加启动描述

---
+++ projects/esbuild-react-demo/README.md
@@ -0,0 +1,7 @@
+# EMP ESBbuild Plugin 
+> 暂时支持react、其他框架有待验证
+
+## 快速启动 验证
++ `cd projects/demo1 && yarn dev --hot`
++ `cd projects/demo2 && yarn dev --hot`
++ `cd projects/esbuild-react-demo && yarn dev --hot`
--- projects/esbuild-react-demo/src/components/SVG.tsx
+++ projects/esbuild-react-demo/src/components/SVG.tsx
@@ -5,9 +5,9 @@ import url, {ReactComponent as Logo} from 'src/assets/logo.svg'
 export default function SVG() {
   return (
     <>
-      <h3>SVG</h3>
+      <h3>.Svg</h3>
       <Logo width="300" className={css['App-logo']} />
-      <h3>IMG SRC</h3>
+      <h3>.IMG Src</h3>
       <img width="300" src={url} alt="logo" />
     </>
   )
