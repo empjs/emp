efde86d43b19c7d8027e004cbb904a6c1d81fd26
ckken <ckken@qq.com>
2020-12-18 14:32:15 +0800

    feat: 增加eslint js支持

---
+++ packages/eslint-config-react/index.js
@@ -0,0 +1,51 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+  ], //使用推荐的React代码检测规范
+  plugins: [],
+  env: {
+    browser: true,
+    node: true,
+  },
+  settings: {
+    //自动发现React的版本，从而进行规范react代码
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
+  },
+  parserOptions: {
+    //指定ESLint可以解析JSX语法
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'prettier/prettier': [
+      'error',
+      {
+        printWidth: 120,
+        semi: false,
+        singleQuote: true,
+        trailingComma: 'all',
+        bracketSpacing: false,
+        jsxBracketSameLine: true,
+        arrowParens: 'avoid',
+        insertPragma: false,
+        tabWidth: 2,
+        useTabs: false,
+        endOfLine: 'auto',
+        usePrettierrc: true,
+      },
+    ],
+  },
+}
---
+++ projects/js-demo/src/bootstrap.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => <>JS DEMO</>
+
+export default App
+
+ReactDOM.render(<App />, document.getElementById('emp-root'))
