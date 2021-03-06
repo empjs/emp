4b611c885d54d0877285cb0724d6cf57c4a217ec
flyyuan <740004544@qq.com>
2021-03-07 00:30:31 +0800

    feat: releases v1.7.13

--- packages/emp-cli/helpers/downloadRemoteFile.js
+++ packages/emp-cli/helpers/downloadRemoteFile.js
@@ -9,25 +9,25 @@
  *        "name": "文件名.d.ts"
  *      }
  *   ]
- * } 
+ * }
  */
 const fs = require('fs')
 const axios = require('axios')
-const { resolveApp } = require('./paths')
-const { remoteBaseUrlList=[] } = require(resolveApp('package.json'))
+const {resolveApp} = require('./paths')
+const {remoteBaseUrlList = []} = require(resolveApp('package.json'))
 
 // 下载远程文件,拉取配置项的URL
 async function downloadRemoteFile(urlList = remoteBaseUrlList) {
   if (urlList.length === 0) return
-  urlList.map(async (item) => {
-    let name = `${a.replace('/', '-')}${/[^*].d.ts/.test(a)?'':'.d.ts'}`
+  urlList.map(async item => {
+    let name = `${a.replace('/', '-')}${/[^*].d.ts/.test(a) ? '' : '.d.ts'}`
     const file = fs.createWriteStream(`${resolveApp('src')}/${name}`)
     const response = await axios({
       url: item.url,
-      method: "GET",
-      responseType: "stream",
+      method: 'GET',
+      responseType: 'stream',
     })
-    
+
     await response.data.pipe(file)
   })
 }
