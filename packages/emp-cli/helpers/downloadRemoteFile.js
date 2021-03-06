95447f738a8acc1a6a8799894179ee7037c81f7c
zxgMaster <32235692+zxgMaster@users.noreply.github.com>
2021-03-06 23:35:16 +0800

    Update downloadRemoteFile.js
    增加导出方法，增加文件名后缀检查

--- packages/emp-cli/helpers/downloadRemoteFile.js
+++ packages/emp-cli/helpers/downloadRemoteFile.js
@@ -5,27 +5,31 @@
  * {
  *  "remoteBaseUrlList": [
  *      {
- *        "url": "https://com/emp.js"
- *        "name": "文件名"
+ *        "url": "https://com/index.d.ts"
+ *        "name": "文件名.d.ts"
  *      }
  *   ]
  * } 
  */
-const fs = require('fs');
-const axios = require('axios');
-const { resolveApp } = require('./paths');
-const { remoteBaseUrlList=[] } = require(resolveApp('package.json'));
+const fs = require('fs')
+const axios = require('axios')
+const { resolveApp } = require('./paths')
+const { remoteBaseUrlList=[] } = require(resolveApp('package.json'))
 
 // 下载远程文件,拉取配置项的URL
 async function downloadRemoteFile(urlList = remoteBaseUrlList) {
-  if (urlList.length === 0) return;
+  if (urlList.length === 0) return
   urlList.map(async (item) => {
-    const file = fs.createWriteStream(`${resolveApp('src')}/${item.name.replace('/', '-')}`);
+    let name = `${a.replace('/', '-')}${/[^*].d.ts/.test(a)?'':'.d.ts'}`
+    const file = fs.createWriteStream(`${resolveApp('src')}/${name}`)
     const response = await axios({
       url: item.url,
       method: "GET",
       responseType: "stream",
-    });
-    await response.data.pipe(file);
-  });
-};
+    })
+    
+    await response.data.pipe(file)
+  })
+}
+
+module.exports = downloadRemoteFile
