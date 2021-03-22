e69a6ec226bef3dd5578b67cbcef97c7cd5e7696
flyyuan <740004544@qq.com>
2021-03-22 18:37:16 +0800

    feat:existsSync

--- packages/emp-cli/helpers/pluginDriver.js
+++ packages/emp-cli/helpers/pluginDriver.js
@@ -1,16 +1,18 @@
 const {resolveApp} = require('../helpers/paths')
 const fs = require('fs')
+const commander = require('commander')
 module.exports = {
   pluginDriver(program) {
+    const appPath = resolveApp('')
+    const extraPath = `${appPath}/emp-extra.js`
     // register 暴露给第三方进行注册 plugin
     const register = plugin => {
-      program.command(plugin.name).action(() => {
-        plugin.exec()
-      })
+      program.command(plugin.name).action(plugin.exec)
     }
-    const appPath = resolveApp('')
-    let empExtra = fs.readFileSync(`${appPath}/emp-extra.js`, 'utf8')
-    eval(empExtra)
+    if (fs.existsSync(extraPath)) {
+      let empExtra = fs.readFileSync(extraPath, 'utf8')
+      eval(empExtra)
+    }
     return program
   },
 }
