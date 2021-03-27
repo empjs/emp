5d1a05564d01583afda490670ca1bbe00f848b2c
liangxiaoming <liangxiaoming@yy.com>
2021-03-27 17:36:24 +0800

    feat: 优化文件存在判断

--- packages/emp-cli/scripts/workspace.js
+++ packages/emp-cli/scripts/workspace.js
@@ -32,59 +32,63 @@ types/
  * 初始化 工作区开发配置， 并追加 gitignore 配置
  */
 const init = async () => {
-  fse.stat(configFilePath, async (err, stats) => {
-    if (stats) {
-      console.log(`${configFilePath}已存在, 创建失败`)
+  const isExists = await fse.pathExists(configFilePath)
+  if (isExists) {
+    console.log(`${configFilePath}已存在, 创建失败`)
+  } else {
+    await fse.createFile(configFilePath)
+    const err = await fse.outputFile(configFilePath, fileTemplate)
+    if (err) {
+      console.log(`${configFilePath}创建失败 ${err}`)
     } else {
-      await fse.createFile(configFilePath)
-      const err = await fse.outputFile(configFilePath, fileTemplate)
-      if (err) {
-        console.log(`${configFilePath}创建失败 ${err}`)
-      } else {
-        console.log(`${configFilePath}创建成功`)
-        const stats = await fse.stat(gitignorePath)
-        if (stats) {
-          fse.appendFile(gitignorePath, ignoreAppendContent, err => {
-            if (err) {
-              console.log(`${gitignorePath}追加内容失败`)
-            } else {
-              console.log(`${gitignorePath}追加内容成功`)
-            }
-          })
-        }
+      console.log(`${configFilePath}创建成功`)
+      const stats = await fse.stat(gitignorePath)
+      if (stats) {
+        fse.appendFile(gitignorePath, ignoreAppendContent, err => {
+          if (err) {
+            console.log(`${gitignorePath}追加内容失败`)
+          } else {
+            console.log(`${gitignorePath}追加内容成功`)
+          }
+        })
       }
     }
-  })
+  }
+}
+
+const getRemoteTsConfig = async () => {
+  const workspaceConfigPath = resolveApp(configFilePath)
+  const isExists = await fse.pathExists(workspaceConfigPath)
+  if (isExists) {
+    let remoteTsConfig = await fse.readFile(workspaceConfigPath, 'utf8')
+    remoteTsConfig = await tsCompile(remoteTsConfig)
+    remoteTsConfig = requireFromString(remoteTsConfig.code, '')
+    console.log('## data', remoteTsConfig)
+    return remoteTsConfig
+  } else {
+    return null
+  }
 }
 
 /**
  * 同步远程文件
  */
 const pullTypes = async () => {
-  console.log('begin pullTypes')
-  const workspaceConfigPath = resolveApp(configFilePath)
-  console.log(`workspaceConfig: ${workspaceConfigPath} `)
-  fse.stat(workspaceConfigPath, async (err, stats) => {
-    if (stats) {
-      let remoteTsConfig = await fse.readFile(workspaceConfigPath, 'utf8')
-      remoteTsConfig = await tsCompile(remoteTsConfig)
-      remoteTsConfig = requireFromString(remoteTsConfig.code, '')
-      console.log('## data', remoteTsConfig)
-      console.log('## data', remoteTsConfig.default.pullConfig)
-      if (remoteTsConfig.default.pullConfig) {
-        for (let key in remoteTsConfig.default.pullConfig) {
-          if (
-            remoteTsConfig.default.pullConfig[key].indexOf('http://') > -1 ||
-            remoteTsConfig.default.pullConfig[key].indexOf('https://') > -1
-          ) {
-            downloadHttpFile(key, remoteTsConfig.default.pullConfig[key])
-          } else {
-            copyLocalFile(key, remoteTsConfig.default.pullConfig[key])
-          }
-        }
+  const remoteTsConfig = await getRemoteTsConfig()
+  if (remoteTsConfig.default.pullConfig && Object.keys(remoteTsConfig.default.pullConfig).length > 0) {
+    for (let key in remoteTsConfig.default.pullConfig) {
+      if (
+        remoteTsConfig.default.pullConfig[key].indexOf('http://') > -1 ||
+        remoteTsConfig.default.pullConfig[key].indexOf('https://') > -1
+      ) {
+        downloadHttpFile(key, remoteTsConfig.default.pullConfig[key])
+      } else {
+        copyLocalFile(key, remoteTsConfig.default.pullConfig[key])
       }
     }
-  })
+  } else {
+    console.log(`请使用 emp workspace:init 指令生成配置文件 并配置pullConfig`)
+  }
 }
 
 /**
@@ -117,6 +121,18 @@ const copyLocalFile = async (path, localUrl) => {
   })
 }
 
+/**
+ * 分发远程文件
+ * @param {*} type
+ */
+const pushTypes = async () => {
+  const remoteTsConfig = await getRemoteTsConfig()
+  console.log('remoteTsConfig.default.pushConfig.localPath', remoteTsConfig)
+  if (!remoteTsConfig.default.pushConfig || remoteTsConfig.default.pushConfig.localPath.length < 1) {
+    console.log('请使用 emp workspace:init 指令生成配置文件 并配置pushConfig')
+  }
+}
+
 module.exports = type => {
   switch (type) {
     case 'init':
@@ -124,6 +140,9 @@ module.exports = type => {
       break
     case 'pullTypes':
       pullTypes()
+      break
+    case 'pushTypes':
+      pushTypes()
       break
   }
 }
