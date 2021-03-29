4e3facaa6fdf129a17fb2730cd9d6d509a846edc
flyyuan <740004544@qq.com>
2021-03-29 21:48:12 +0800

    feat: 全局命令

--- packages/emp-cli/helpers/pluginDriver.js
+++ packages/emp-cli/helpers/pluginDriver.js
@@ -1,10 +1,24 @@
 const {resolveApp} = require('../helpers/paths')
 const fs = require('fs')
 const commander = require('commander')
+const child_process = require('child_process')
 module.exports = {
   pluginDriver(program) {
     const appPath = resolveApp('')
     const extraPath = `${appPath}/emp-extra.js`
+
+    const globalPlugin = () => {
+      const empPlugin = []
+      const globalModules = child_process.execSync('npm root -g').toString()
+      const modulesPath = fs.readdirSync(globalModules.trim())
+      modulesPath.forEach(item => {
+        if (item.match('emp-')) {
+          empPlugin.push(`${globalModules.trim()}/${item}/index.js`)
+        }
+      })
+      return empPlugin
+    }
+
     const optionsMerge = options => {
       let optionCall = ''
       if (options) {
@@ -14,6 +28,7 @@ module.exports = {
       }
       return optionCall
     }
+
     // register 暴露给第三方进行注册 plugin
     const register = plugin => {
       eval(`program.command(plugin.name)${optionsMerge(plugin.options)}.action(${plugin.exec})`)
@@ -22,6 +37,13 @@ module.exports = {
       let empExtra = fs.readFileSync(extraPath, 'utf8')
       eval(empExtra)
     }
+
+    const globalPluginPath = globalPlugin()
+    globalPluginPath.forEach(pluginPath => {
+      const globalPlugin = fs.readFileSync(pluginPath, 'utf8')
+      eval(globalPlugin)
+    })
+
     return program
   },
 }
