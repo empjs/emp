86b8efa537060bf49ee8e0c5b31408b47e92b174
flyyuan <740004544@qq.com>
2021-04-06 18:15:34 +0800

    fix: 开发全局插件可以本地启动

--- packages/emp-cli/helpers/pluginDriver.js
+++ packages/emp-cli/helpers/pluginDriver.js
@@ -2,27 +2,40 @@ const {resolveApp} = require('../helpers/paths')
 const fs = require('fs')
 const commander = require('commander')
 const child_process = require('child_process')
+const path = require('path')
+
 module.exports = {
   pluginDriver(program) {
     const appPath = resolveApp('')
-    const extraPath = `${appPath}/emp-extra.js`
+    const extraPath = path.join(appPath, 'emp-extra.js')
 
     const globalPlugin = () => {
       const empPlugin = []
       const npmGlobalModules = child_process.execSync('npm root -g').toString()
       const yarnGlobalModules = child_process.execSync('yarn global dir').toString()
-      const yarnModulesPath = fs.readdirSync(`${yarnGlobalModules.trim()}/node_modules`)
+      const yarnModulesPath = fs.readdirSync(path.join(yarnGlobalModules.trim(), 'node_modules'))
       const npmModulesPath = fs.readdirSync(npmGlobalModules.trim())
       npmModulesPath.forEach(item => {
         if (item && item.match('emp-plugin-')) {
-          empPlugin.push(`${npmGlobalModules.trim()}/${item}/index.js`)
+          empPlugin.push(path.join(npmGlobalModules.trim(), item, 'index.js'))
         }
       })
       yarnModulesPath.forEach(item => {
         if (item && item.match('emp-plugin-')) {
-          empPlugin.push(`${yarnGlobalModules.trim()}/node_modules/${item}/index.js`)
+          empPlugin.push(path.join(yarnGlobalModules.trim(), 'node_modules', item, 'index.js'))
         }
       })
+
+      const cwd = process.cwd()
+      if (cwd.match('emp-plugin-')) {
+        const localPath = cwd.split(path.sep)
+        localPath.forEach((item, index) => {
+          if (item.indexOf('emp-plugin-') > -1) {
+            const pluginPath = path.join(path.sep, ...localPath.slice(0, index + 1))
+            empPlugin.push(path.join(pluginPath, 'index.js'))
+          }
+        })
+      }
       return empPlugin
     }
 
@@ -38,7 +51,15 @@ module.exports = {
 
     // register 暴露给第三方进行注册 plugin
     const registerCommand = plugin => {
-      eval(`program.command(plugin.name)${optionsMerge(plugin.options)}.action(${plugin.exec})`)
+      if (plugin.description) {
+        eval(
+          `program.command(plugin.name).description(plugin.description)${optionsMerge(plugin.options)}.action(${
+            plugin.exec
+          })`,
+        )
+      } else {
+        eval(`program.command(plugin.name)${optionsMerge(plugin.options)}.action(${plugin.exec})`)
+      }
     }
     if (fs.existsSync(extraPath)) {
       let empExtra = fs.readFileSync(extraPath, 'utf8')
--- projects/demo1/emp-extra.js
+++ projects/demo1/emp-extra.js
@@ -1,6 +1,7 @@
 // 局部插件开发例子
 registerCommand({
   name: 'hello',
+  description: 'It is description',
   options: [{name: '-i, --item <item>', description: 'flavour of pizza'}],
   exec: ({item}) => {
     console.log(`hello ${item}`)
