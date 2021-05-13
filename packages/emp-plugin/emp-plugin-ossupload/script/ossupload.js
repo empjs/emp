74983c4aadebf2aa154b1ddfe24398cdee8f7278
JeremyLiang <398921432@qq.com>
2021-05-13 21:59:46 +0800

    Feature/jeremy ossupload (#147)
    * feat: add ossupload plugin
    
    * feat: 优化插件指令
    
    * feat: 完善上传oss功能
    
    * feat: 增加unlink 逻辑
    
    * feat: 完成上传插件

---
+++ packages/emp-plugin/emp-plugin-ossupload/script/ossupload.js
@@ -0,0 +1,42 @@
+const FormData = require('form-data')
+const fs = require('fs')
+const download = require('../helps/download')
+const axios = require('axios')
+
+const uploadOss = async (name, version) => {
+  console.log(`name,version`, name, version)
+  const remoteUrl = `https://npm-registry.yy.com/${name}/-/${name}-${version}.tgz`
+  const uploadUrl = `https://koa-simple-test.bdgamelive.com/ossUpload`
+  console.log('开始下载', remoteUrl)
+  try {
+    const downloadResult = await download(`${name}-${version}.tgz`, remoteUrl)
+    const formData = new FormData()
+    const stream = fs.createReadStream(downloadResult.filePath, {highWaterMark: 1})
+
+    formData.append('custompath', `/unpkg/${name.replace(/\//g, '-')}/${version}/`)
+    formData.append('file', stream)
+    const formHeaders = formData.getHeaders()
+    console.log('上传包名:', name)
+    console.log('上传版本号:', version)
+    console.log('开始上传..')
+    axios
+      .post(uploadUrl, formData, {
+        headers: {
+          ...formHeaders,
+        },
+      })
+      .then(response => {
+        console.log('upload then', response?.status)
+      })
+      .catch(error => {
+        console.log('upload error', error)
+      })
+    fs.unlinkSync(downloadResult.filePath)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+module.exports = (name, version) => {
+  uploadOss(name, version)
+}
