87fa862a24ca6d85d59dbff5dab432ab163fedb1
yangshangzhi <yangshangzhi@yy.com>
2021-05-14 19:43:17 +0800

    feat: modify plugin

--- packages/emp-plugin/emp-plugin-git-version/cli.js
+++ packages/emp-plugin/emp-plugin-git-version/cli.js
@@ -6,29 +6,36 @@ const path = require('path')
 const fs = require('fs-extra')
 const execa = require('execa')
 const [arg0, arg1, regexpName, replaceName] = process.argv
-console.log('args: ', regexpName, replaceName)
 
 const appDirectory = fs.realpathSync(process.cwd())
 const resolveApp = relativePath => path.resolve(appDirectory, relativePath)
 
 function getGitBranch() {
-  const res = execa.commandSync('git rev-parse --abbrev-ref HEAD')
-  return res.stdout
+  try {
+    const res = execa.commandSync('git rev-parse --abbrev-ref HEAD')
+    return res.stdout
+  } catch (e) {}
 }
 function init() {
   const result = fs.readFileSync(resolveApp('package.json'))
   const content = JSON.parse(result)
   const gitBranchName = getGitBranch()
   let version = content.version
-  console.log('currentBranchName: ', gitBranchName)
   try {
     const matchResult = gitBranchName.match(new RegExp(regexpName, 'i'))
-    if (regexpName && matchResult) {
+    if (regexpName && matchResult && gitBranchName) {
+      console.log('args: ', regexpName, replaceName)
+      console.log('currentBranchName: ', gitBranchName)
       version = gitBranchName.replace(new RegExp(regexpName, 'i'), replaceName || '')
-      console.log('version: ', version)
+      console.log('modify version: ', version)
       content.version = version
       fs.writeFileSync(resolveApp('package.json'), JSON.stringify(content, null, 2))
+      console.log('success')
+    } else {
+      console.log('done')
     }
   } catch (e) {}
 }
 init()
+
+module.exports = program => {}
