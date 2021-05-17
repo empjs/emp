fc0ed172749c711e2d1d06db1a3b3b266543e8d0
flyyuan <740004544@qq.com>
2021-05-17 15:14:45 +0800

    fix: 固定 webpack-dev-server 版本和 loose: false

--- packages/emp-cli/webpack/config/module.js
+++ packages/emp-cli/webpack/config/module.js
@@ -69,7 +69,7 @@ module.exports = (env, config, {hot}) => {
                     },
                   ],
                   [require('@babel/plugin-proposal-decorators').default, {legacy: true}],
-                  [require('@babel/plugin-proposal-class-properties').default, {loose: true}],
+                  [require('@babel/plugin-proposal-class-properties').default, {loose: false}],
                 ].filter(Boolean),
               },
             },
