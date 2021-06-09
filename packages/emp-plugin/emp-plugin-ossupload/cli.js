1e747301cc2ca2710a8888a166c682da72fcee5f
doerme <398921432@qq.com>
2021-06-09 11:30:54 +0800

    feat: remove oss upload

--- packages/emp-plugin/emp-plugin-ossupload/cli.js
+++
@@ -1,9 +0,0 @@
-module.exports = program => {
-  program
-    .command('ossupload [packageName] [packageVersion]')
-    .description(`同步yynpm资源到阿里云: ossupload [包名] [版本号] 默认获取当前包信息`)
-    .action((packageName, packageVersion) => {
-      console.log('ossupload type:', packageName, packageVersion)
-      require('./script/ossupload')(packageName, packageVersion)
-    })
-}
