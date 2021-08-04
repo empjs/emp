94b1c3ea3fbb7b8ef0a04b5824cf6d30a2d4af2b
ckken <ckken@qq.com>
2021-08-04 21:53:17 +0800

    chore: 增加实验选项支持

--- packages/emp-cli/webpack/config/style.js
+++ packages/emp-cli/webpack/config/style.js
@@ -166,6 +166,15 @@ module.exports = (env, config, args) => {
         ignoreOrder: true,
         filename: 'static/css/[name].[contenthash:8].css',
         chunkFilename: 'static/css/[name].[contenthash:8].chunk.css',
+        /**
+          experimentalUseImportModule
+          https://github.com/webpack-contrib/mini-css-extract-plugin#experimentalUseImportModule
+          Use an experimental webpack API to execute modules instead of child compilers.
+          This improves performance and memory usage a lot, but isn't as stable as the normal approach.
+          When combined with experiments.layers, this adds a layer option to the loader options to specify the layer of the css execution.
+          You need to have at least webpack 5.33.2.
+         */
+        // experimentalUseImportModule: true,
       },
     ])
   }
