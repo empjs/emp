dc00c292734c7a101cc87faca15a434e3a98930e
flyyuan <740004544@qq.com>
2021-10-20 10:51:41 +0800

    feat: init 增加loading

--- packages/emp-cli/helpers/downloadRepo.js
+++ packages/emp-cli/helpers/downloadRepo.js
@@ -1,17 +1,20 @@
 const git = require('git-promise') // 运行git命令
 const chalk = require('chalk')
 const fs = require('fs-extra')
+const ora = require('ora')
 
 async function downloadRepo(repoPath, localPath, branch) {
+  const spinner = ora('正在拉取模板...').start()
   const _branch = branch ? `-b ${branch} --` : '--'
   const _repoPath = `clone ${_branch} ${repoPath} ./${localPath}`
   if (!fs.existsSync(localPath)) {
     await git(_repoPath)
     fs.removeSync(`./${localPath}/.git`)
-    console.log('初始化完成，请输入:')
+    spinner.succeed('init 成功,执行以下命令启动项目')
     console.log(chalk.green(`cd ${localPath} && yarn && yarn dev`))
   } else {
     console.log('已存在指定目录')
+    spinner.warn('init 失败')
   }
 }
 
