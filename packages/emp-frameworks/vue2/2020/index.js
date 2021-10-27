a296e39ce87f058b1e0fdd0e0f60c8612295938e
xuhongbin <xuhongbin@yy.com>
2021-10-27 12:37:45 +0800

    feat: 迁移empfe项目到 emp

--- packages/emp-frameworks/vue2/2020/index.js
+++ packages/emp-frameworks/vue2/2020/index.js
@@ -1,18 +1,13 @@
-// const path = require('path')
+const path = require('path')
 module.exports = fn => ec => {
   const {config} = ec
   config.resolve.alias.set('vue', '@vue/runtime-dom')
-  config.plugin('vue-loader-plugin').use(require('vue-loader').VueLoaderPlugin, [])
+  config.plugin('vue_v2').use(require('vue-loader').VueLoaderPlugin, [])
   config.module
-    .rule('vue-loader')
+    .rule('vue_v2')
     .test(/\.vue$/)
     .use('vue-loader')
     .loader(require.resolve('vue-loader'))
   config.resolve.alias.set('vue$', 'vue/dist/vue.esm.js').clear()
-  //
-  const svgRule = config.module.rule('svg')
-  svgRule.uses.clear()
-  svgRule.use('vue-svg-loader').loader(require.resolve('vue-svg-loader'))
-  //
   return fn && fn(ec)
 }
