f2ddfd92b1df9939229ee030488a5e74bde207b6
xuhongbin <xuhongbin@yy.com>
2021-11-10 17:09:30 +0800

    chore: up

--- projects/demo/emp-config.js
+++ projects/demo/emp-config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     // hot: 'only',
   },
   build: {
-    // target: 'es5',
+    target: 'es5',
     sourcemap: false,
     // outDir: 'build',
   },
--- projects/empty-project/emp-config.js
+++ projects/empty-project/emp-config.js
@@ -2,4 +2,7 @@ const {defineConfig} = require('@efox/emp')
 const pluginBabelReact = require('@efox/plugin-babel-react').default
 module.exports = defineConfig({
   plugins: [pluginBabelReact],
+  build: {
+    // target: 'es2015',
+  },
 })
