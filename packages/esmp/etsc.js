78c8d771d92219dffaa8ad0bc2896e33e0d591a6
ckken <ckken@qq.com>
2021-11-13 03:27:36 +0800

    chore: up

--- packages/esmp/etsc.js
+++ packages/esmp/etsc.js
@@ -6,6 +6,9 @@ const glob = require('tiny-glob')
 //
 const esbuild = require('esbuild')
 const {dtsPlugin} = require('esbuild-plugin-d.ts')
+const {sassPlugin} = require('@es-pack/esbuild-sass-plugin')
+const {lessLoader} = require('esbuild-plugin-less')
+const cssModulesPlugin = require('esbuild-css-modules-plugin')
 //
 const isDEV = process.env.NODE_ENV === 'development'
 ;(async (src = 'src', watch = isDEV, out = 'dist') => {
@@ -25,15 +28,17 @@ const isDEV = process.env.NODE_ENV === 'development'
     platform: 'node',
     // bundle: true,
     watch,
+    bundle: true,
     // minify:true,
     tsconfig: tsconfigPath,
-    plugins: [dtsPlugin()],
+    plugins: [dtsPlugin(), sassPlugin(), lessLoader(), cssModulesPlugin()],
     loader: {
       '.tsx': 'tsx',
       '.ts': 'ts',
       '.jsx': 'jsx',
       '.js': 'js',
       '.json': 'json',
+      '.css': 'css',
     },
   })
 })()
