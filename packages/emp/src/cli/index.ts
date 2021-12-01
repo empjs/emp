8dfbe1e20e020e932b9cc20430d3fde275aa2289
xuhongbin <xuhongbin@baidu.com>
2021-12-01 19:14:11 +0800

    refactor: 增加模式层

--- packages/emp/src/cli/index.ts
+++ packages/emp/src/cli/index.ts
@@ -5,29 +5,37 @@ import {cliOptionsType, modeType} from 'src/types'
 import configPlugins from 'src/config/plugins'
 import configChain from 'src/config/chain'
 class EMPScript {
+  // 执行文件名称
+  name = ''
   constructor() {}
   /**
    * 执行命令相关脚本
    * @param name
    */
   async exec(name: string, mode: modeType, cliOptions: cliOptionsType, pkg: any): Promise<void> {
+    this.name = name
     // 全局变量实例化 store & config
     await store.setup(mode, cliOptions, pkg)
-    if (store.config.build.lib) {
-      // 库模式实例化
-      await (await import('src/webpack/wpLibMode')).default.setup()
-      name = 'build'
-    } else {
-      // webpack实例化
-      await (await import('src/webpack')).default.setup()
-    }
+    //模式层
+    await this.selectMode()
     // 初始化所有 EMP Plugins
     await configPlugins.setup()
     // webpack Chain
     await configChain.setup()
     // 执行cli脚本
-    const cilScript = await import(`./${name}`)
+    const cilScript = await import(`./${this.name}`)
     await cilScript.default.setup()
+  }
+  async selectMode() {
+    if (store.config.build.lib) {
+      // 库模式实例化
+      await (await import('src/webpack/wpLibMode')).default.setup()
+      // 执行 build操作
+      this.name = 'build'
+    } else {
+      // web实例化
+      await (await import('src/webpack')).default.setup()
+    }
   }
 }
 
