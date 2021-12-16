8a8633047e1778e5b60db062d131c3080fee9a19
flyyuan <740004544@qq.com>
2021-12-16 22:42:45 +0800

    feat: http 换成 axios

--- packages/emp/src/helper/downloadDts.ts
+++ packages/emp/src/helper/downloadDts.ts
@@ -3,51 +3,34 @@ import http from 'http'
 import https from 'https'
 import fs from 'fs-extra'
 import path from 'path'
+import axios from 'axios'
 
-function downloadFileAsync(uri: string, filePath: string, fileName: string, alias: string, baseName: string) {
-  const url = new URL(uri)
-  const client = url.protocol == 'https:' ? https : http
-  return new Promise((resolve, reject) => {
-    client.get(uri, res => {
-      if (res.statusCode !== 200) {
-        reject(res.statusCode)
-        return
-      }
-      // 确保路径存在
-      fs.ensureDirSync(filePath)
-      const fullPath = path.resolve(filePath, fileName)
-      const file = fs.createWriteStream(fullPath)
-      res.on('end', () => {})
-      // 进度、超时等
-      file
-        .on('finish', () => {
-          console.log(`finish ./${filePath}/${fileName}`)
-          file.close(resolve)
-        })
-        .on('error', err => {
-          fs.unlink(filePath, e => {
-            reject(e)
-          })
-          reject(err.message)
-        })
-      res.setEncoding('utf8')
-      res.on('data', function (data) {
-        let newData = ''
-        // 替换 remote 别名
-        const regSingleQuote = new RegExp(`'${baseName}`, 'g')
-        const regDoubleQuote = new RegExp(`"${baseName}`, 'g')
-        newData = data.replace(regSingleQuote, `'${alias}`)
-        newData = newData.replace(regDoubleQuote, `"${alias}`)
-        fs.writeFileSync(fullPath, newData, 'utf8')
-      })
-    })
+async function downloadFileAsync(uri: string, filePath: string, fileName: string, alias: string, baseName: string) {
+  const httpsAgent = new https.Agent({
+    rejectUnauthorized: false,
   })
+  axios.defaults.httpsAgent = httpsAgent
+  try {
+    const {data} = await axios.get(uri)
+    let newData = ''
+    // 替换 remote 别名
+    const regSingleQuote = new RegExp(`'${baseName}`, 'g')
+    const regDoubleQuote = new RegExp(`"${baseName}`, 'g')
+    newData = data.replace(regSingleQuote, `'${alias}`)
+    newData = newData.replace(regDoubleQuote, `"${alias}`)
+    const fullPath = path.resolve(filePath, fileName)
+    console.log(`${uri} --> ${fullPath}`)
+    fs.writeFileSync(fullPath, newData, 'utf8')
+  } catch (error) {
+    console.log(error)
+    console.log(`${uri} --> network error`)
+  }
 }
 
 /**
  * 下载 remote 的 d.ts
  */
-const downloadDts = () => {
+const downloadDts = async () => {
   const remotes = store.empShare.moduleFederation.remotes
   if (remotes) {
     for (const [key, value] of Object.entries(remotes)) {
@@ -56,10 +39,12 @@ const downloadDts = () => {
         const baseUrl = value.split('@')[1]
         const {outDir, typesOutDir} = store.config.build
         // typesOutDir 可以独立设置 但是必须在outDir里，否则影响DTS同步
-        const dtsUrl = baseUrl.replace('/emp.js', `/${typesOutDir.replace(outDir, '')}/index.d.ts`)
-        downloadFileAsync(dtsUrl, store.config.typingsPath, `${key}.d.ts`, key, baseName)
+        const dtsUrl = baseUrl.replace('/emp.js', `${typesOutDir.replace(outDir, '')}/index.d.ts`)
+        await downloadFileAsync(dtsUrl, store.config.typingsPath, `${key}.d.ts`, key, baseName)
       }
     }
+  } else {
+    console.log('No found remotes')
   }
 }
 export {downloadDts}
