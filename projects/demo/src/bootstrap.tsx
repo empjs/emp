791e810ce0f4d832027032f1f1ad987b6e000bc1
xuhongbin <xuhongbin@baidu.com>
2022-03-18 15:39:51 +0800

    feat: eslint react v2.0.3

--- projects/demo/src/bootstrap.tsx
+++ projects/demo/src/bootstrap.tsx
@@ -2,3 +2,9 @@ import {render} from 'react-dom'
 import RouterComp from 'src/RouterComp'
 
 render(<RouterComp />, document.getElementById('emp-root'))
+
+// class TreasureInfoStore {
+//   awardDuration = 2000 // 中奖动画世界
+//   treasureInfo: undefined // 宝藏页面信息
+//   treasureLevel = 0 // 当前搜寻等级   0.低级   1.高级
+// }
