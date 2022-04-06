23486afe6facf5e119eeed4f6dd0035fa8da3830
xuhongbin <xuhongbin@baidu.com>
2022-04-06 11:14:48 +0800

    chore: 升级到 swc 1.2.163 还没解决source map 问题

--- projects/demo/src/bootstrap.tsx
+++ projects/demo/src/bootstrap.tsx
@@ -3,8 +3,11 @@ import RouterComp from 'src/RouterComp'
 
 render(<RouterComp />, document.getElementById('emp-root'))
 
-// class TreasureInfoStore {
-//   awardDuration = 2000 // 中奖动画世界
-//   treasureInfo: undefined // 宝藏页面信息
-//   treasureLevel = 0 // 当前搜寻等级   0.低级   1.高级
-// }
+class TreasureInfoStore {
+  awardDuration = 2000 // 中奖动画世界
+  treasureInfo: string | undefined // 宝藏页面信息
+  treasureLevel = 0 // 当前搜寻等级   0.低级   1.高级
+  constructor() {
+    this.treasureInfo = 'abc'
+  }
+}
