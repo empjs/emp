30c437adb2be5c3aa4aa9d02b2bb24e890c8b114
vyron <fiee_vyron@163.com>
2022-07-15 13:55:56 +0800

    feat: 增加 webpack 导出

--- packages/emp/src/index.ts
+++ packages/emp/src/index.ts
@@ -1,3 +1,9 @@
-export {defineConfig, EMPConfig, ResovleConfig, EMPConfigExport} from 'src/config'
-export {ConfigPluginOptions} from 'src/config/plugins'
-export {default as empStore} from 'src/helper/store'
+export {
+	defineConfig,
+	EMPConfig,
+	ResovleConfig,
+	EMPConfigExport,
+} from "src/config";
+export { ConfigPluginOptions } from "src/config/plugins";
+export { default as empStore } from "src/helper/store";
+export { default as webpack } from "webpack";
