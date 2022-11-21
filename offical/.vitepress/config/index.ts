8edb06a1b2b45be6db32eb6f815885d3d71db448
xuhongbin <xuhongbin@yy.com>
2022-11-21 11:09:47 +0800

    docs: up

---
+++ offical/.vitepress/config/index.ts
@@ -0,0 +1,50 @@
+export const members = [
+	{
+		avatar: 'https://avatars.githubusercontent.com/u/3890513?v=4',
+		name: 'Ken Xu',
+		title: 'Creator',
+		links: [
+			{ icon: 'github', link: 'https://github.com/ckken' },
+			{icon:'twitter',link:'https://juejin.cn/column/7019658827918213157'}
+
+		]
+	},
+	{
+		avatar: 'https://avatars.githubusercontent.com/u/2680224?v=4',
+		name: 'JeremyLiang',
+		title: 'Developer',
+		links: [
+			{ icon: 'github', link: 'https://github.com/doerme' },
+
+		]
+	},
+	{
+		avatar: 'https://avatars.githubusercontent.com/u/19996552?v=4',
+		name: 'Benny Shi',
+		title: 'Developer',
+		links: [
+			{ icon: 'github', link: 'https://github.com/flyyuan' },
+
+		]
+	},
+	{
+		avatar: 'https://avatars.githubusercontent.com/u/20643950?v=4',
+		name: 'Ron0115',
+		title: 'Developer',
+		links: [
+			{ icon: 'github', link: 'https://github.com/ron0115' },
+
+		]
+	},
+]
+export const links = [
+{ name: 'bdgamelive', img: '/img/logo/bdgamelive.png' },
+{ name: 'yy', img: '/img/logo/yylive.png' },
+{ name: 'joyy', img: '/img/logo/joyy.png' },
+{ name: 'shopline', img: '/img/logo/shopline.png' },
+{ name: 'wanke', img: '/img/logo/wanke.png' },
+{ name: 'zuoyebang', img: '/img/logo/zuoyebang.jpeg' },
+{ name: 'yomi', img: '/img/logo/yomi.png' },
+{ name: 'yokaihei', img: '/img/logo/yokaihei.png' },
+{ name: 'zhuiwan', img: '/img/logo/zhuiwan.png' },
+]
