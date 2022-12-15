62001154a44a601379c2055abf3ceb1ed8ad3b13
xuhongbin <xuhongbin@yy.com>
2022-12-15 11:01:25 +0800

    docs: up

--- offical/.vitepress/config.js
+++ offical/.vitepress/config.js
@@ -9,7 +9,7 @@ export default defineConfig(() => ({
 	// cleanUrls: 'without-subfolders',
 	//
 	head: [
-		['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo/logo.png' }],
+		['link', { rel: 'icon', type: 'image/svg+xml', href: '/favicon.ico' }],
 		['meta', { name: 'theme-color', content: '#3700b3' }],
 		['script',{},`var _hmt = _hmt || [];
 		(function() {
