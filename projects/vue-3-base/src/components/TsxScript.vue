08a5b66fc32569affcc5ce9fdcaa313da48de16c
MAXLZ <2251683416@qq.com>
2022-12-26 00:22:14 +0800

    fix: 修复vue3中使用lang=tsx时的报错问题

---
+++ projects/vue-3-base/src/components/TsxScript.vue
@@ -0,0 +1,28 @@
+<script lang="tsx">
+import {defineComponent, ref} from 'vue'
+
+export default defineComponent({
+  name: 'TsxScript',
+  setup() {
+    const count = ref(0)
+    function add() {
+      count.value++
+    }
+
+    return () => (
+      <>
+        <p>============ lang=tsx component start =============</p>
+        <AButton type="primary" onClick={add}>add</AButton>
+        <span class="value">count: {count.value}</span>
+        <p>============ lang=tsx component end =============</p>
+      </>
+    )
+  }
+})
+</script>
+
+<style lang="less" scoped>
+.value {
+  margin-left: 10px;
+}
+</style>
