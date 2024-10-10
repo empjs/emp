7c5583493d47bd2596a8f396e30fa99646077658
doerme <398921432@qq.com>
2024-10-10 16:07:39 +0800

    feat: app upgrade

--- demos/react16_18/react-16-app/emp.config.ts
+++ demos/react16_18/react-16-app/emp.config.ts
@@ -3,7 +3,7 @@ import pluginReact from '@empjs/plugin-react'
 import {pluginRspackEmpShare} from '@empjs/share'
 //
 export default defineConfig(store => {
-  const mfhost = `https://e8e7be5c.emp-demo-react18-host.pages.dev/emp.js`
+  const mfhost = `https://ae41944e.emp-demo-react18-host.pages.dev/demos/react18/react-host/dist/emp.js`
   const frameworkLib = `https://unpkg.com/@empjs/libs-18@0.0.1/dist`
   return {
     // html: {
--- demos/react18/react-host/src/App.tsx
+++ demos/react18/react-host/src/App.tsx
@@ -1,5 +1,13 @@
+import {CountComp} from './CountComp'
 type HostType = {
   name?: string
 }
-const App = ({name}: HostType) => <h1>React 18 Runtime Host: {name ? name : ''}</h1>
+const App = ({name}: HostType) => {
+  return (
+    <>
+      <h1>React 18 Runtime Host: {name ? name : ''}</h1>
+      <CountComp />
+    </>
+  )
+}
 export default App
