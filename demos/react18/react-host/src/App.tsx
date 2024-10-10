81036bbe92f523580ee5b18a3295e3266b9c9e5a
doerme <398921432@qq.com>
2024-10-10 17:14:51 +0800

    chore: fill host demo

--- demos/react18/react-host/src/App.tsx
+++ demos/react18/react-host/src/App.tsx
@@ -1,11 +1,13 @@
 import {CountComp} from './CountComp'
 type HostType = {
   name?: string
+  component?: React.ElementType
 }
-const App = ({name}: HostType) => {
+const App = ({name, component}: HostType) => {
   return (
     <>
       <h1>React 18 Runtime Host: {name ? name : ''}</h1>
+      {component && <component />}
       <CountComp />
     </>
   )
