c91e8bd3aa7c9591a2a26baac571c2fc8284ba76
xuhongbin <xuhongbin@yy.com>
2024-10-11 12:11:06 +0800

    feat: up

--- demos/react16_18/react-16-app/src/bootstrap.tsx
+++ demos/react16_18/react-16-app/src/bootstrap.tsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 // eslint-disable-next-line react/no-deprecated
-ReactDOM.render(<App />, document.getElementById('emp-root'))
+ReactDOM.render(<>abc</>, document.getElementById('emp-root'))
