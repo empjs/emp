75a7dc661e1db7401f51f7201838e35608521f2e
doerme <398921432@qq.com>
2024-10-11 12:16:29 +0800

    chore: biome format

--- demos/react16_18/react-16-app/emp.config.ts
+++ demos/react16_18/react-16-app/emp.config.ts
@@ -3,9 +3,9 @@ import pluginReact from '@empjs/plugin-react'
 import {pluginRspackEmpShare} from '@empjs/share'
 //
 export default defineConfig((store) => {
-  const mfhost = `https://emp-demo-react18-host.pages.dev/demos/react18/react-host/dist/emp.js`
+  const mfhost = "https://emp-demo-react18-host.pages.dev/demos/react18/react-host/dist/emp.js"
   // const mfhost = `http://localhost:1802/emp.js`
-  const frameworkLib = `https://unpkg.com/@empjs/libs-18@0.0.1/dist`
+  const frameworkLib = "https://unpkg.com/@empjs/libs-18@0.0.1/dist"
   return {
     // html: {
     //   title: pkg.name,
@@ -16,7 +16,7 @@ export default defineConfig((store) => {
         empRuntime: {
           frameworkLib,
           frameworkGlobal: 'EMP_ADAPTER_REACT',
-          runtimeLib: `https://unpkg.yy.com/@empjs/share@3.1.2/output/full.js`,
+          runtimeLib: "https://unpkg.com/@empjs/share@3.1.2/output/full.js",
         },
       }),
     ],
