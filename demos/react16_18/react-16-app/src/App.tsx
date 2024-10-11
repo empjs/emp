3485f16c78495993e63aaa48c7dab268716360a9
xuhongbin <xuhongbin@yy.com>
2024-10-11 12:20:31 +0800

    feat: up

--- demos/react16_18/react-16-app/src/App.tsx
+++ demos/react16_18/react-16-app/src/App.tsx
@@ -1,14 +1,10 @@
-import React, { useEffect, useState, version } from 'react'
-import ReactDOM from 'react-dom'
-import { Card, CountComp as CountComp16, ShowCountComp as ShowCountComp16 } from './CountComp'
-import './index.scss'
-import './normalize.css'
+
 
 
 const App = () => {
   return (
     <>
-      <h1>App React Version {version}</h1>
+      <h1>App React Version</h1>
     </>
   )
 }
