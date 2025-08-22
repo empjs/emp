ed7244393ed093b65e28a919a683912f3e2bc4c8
xuhongbin <xuhongbin@yy.com>
2025-08-22 12:23:59 +0800

    feat: biome v2.2.0

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({watch}) => {
     banner: ({format}) => {
       if (format === 'esm') {
         return {
-          js: `import {createRequire as __createRequire} from 'module';var require=__createRequire(import\.meta.url);`,
+          js: `import {createRequire as __createRequire} from 'module';var require=__createRequire(import.meta.url);`,
         }
       }
     },
