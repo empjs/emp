ee2ea80575bf5eccc9eef9b764e4fe501c819321
xuhongbin <xuhongbin@yy.com>
2025-09-12 16:46:20 +0800

    fix(mf-app): 更新reactRouter库引用为unpkg CDN地址

--- projects/mf-app/emp.config.ts
+++ projects/mf-app/emp.config.ts
@@ -30,7 +30,8 @@ export default defineConfig(store => {
             global: 'EMP_ADAPTER_REACT',
             // libs: [`https://unpkg.com/@empjs/cdn-react@0.18.0/dist/reactRouter.${store.mode}.umd.js`],
             // libs: [`https://unpkg.com/@empjs/cdn-react@0.18.0/dist/reactRouter.development.umd.js`],
-            libs: [`http://${store.server.ip}:1900/reactRouter.${store.mode}.umd.js`],
+            libs: [`https://unpkg.com/@empjs/cdn-react@0.19.1/dist/reactRouter.${store.mode}.umd.js`],
+            // libs: [`http://${store.server.ip}:1900/reactRouter.${store.mode}.umd.js`],
           },
           runtime: {
             lib: `https://unpkg.com/@empjs/share@3.10.0-beta.6/output/sdk.js`,
