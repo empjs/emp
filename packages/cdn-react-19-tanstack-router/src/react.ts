8d9f7fdf071016a9713ffe17c3d6c79dc718bc1a
xuhongbin <xuhongbin@yy.com>
2025-09-12 16:52:36 +0800

    feat: 新增React 19与TanStack Router的CDN打包配置
    添加React 19和TanStack Router的CDN打包支持，包括配置文件、依赖项和构建脚本

---
+++ packages/cdn-react-19-tanstack-router/src/react.ts
@@ -0,0 +1,7 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+export {jsxDEV} from 'react/jsx-dev-runtime'
+export * from 'react/jsx-runtime'
+export * from 'react-dom/client'
+export {React, ReactDOM}
