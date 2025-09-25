589d8e7fd68fb6f3b783014242d38b7afa3b9f4b
xuhongbin <xuhongbin@yy.com>
2025-09-25 18:00:41 +0800

    up

--- projects/adapter-app/src/components/Info.tsx
+++ projects/adapter-app/src/components/Info.tsx
@@ -20,29 +20,27 @@ export const Box = ({title, desc, children, className = ''}: BoxProps): JSX.Elem
   </div>
 )
 
-interface ReactInfoProps {
-  desc: string
-  children?: React.ReactNode
-}
-
+export const ReactVersionInfo = () => (
+  <span
+    style={{
+      position: 'absolute',
+      top: '5px',
+      right: '10px',
+      fontSize: '10px',
+      backgroundColor: '#e9f0f8',
+      color: '#4a90e2',
+      padding: '2px 6px',
+      borderRadius: '10px',
+      border: '1px solid #6ba4e5',
+      fontWeight: 'normal',
+    }}
+  >
+    React {React.version}
+  </span>
+)
 export const ReactInfo = ({desc, children, title, className}: BoxProps): JSX.Element => (
   <Box className={`${style.reactInfo} ${className}`} title={title} desc={desc}>
-    <span
-      style={{
-        position: 'absolute',
-        top: '5px',
-        right: '10px',
-        fontSize: '10px',
-        backgroundColor: '#e9f0f8',
-        color: '#4a90e2',
-        padding: '2px 6px',
-        borderRadius: '10px',
-        border: '1px solid #6ba4e5',
-        fontWeight: 'normal',
-      }}
-    >
-      React {React.version}
-    </span>
+    <ReactVersionInfo />
     {children && <Box>{children}</Box>}
   </Box>
 )
