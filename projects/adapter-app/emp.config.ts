0cf8760613e22a2f8fc3ccd9cf53958d433de2ca
xuhongbin <xuhongbin@yy.com>
2025-09-26 12:26:10 +0800

    fix(adapter-app): 更新部署环境下的远程主机地址为https

--- projects/adapter-app/emp.config.ts
+++ projects/adapter-app/emp.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(store => {
   const isDeploy = store.cliOptions.envVars?.deploy === 'cloudflare'
   const base = isDeploy ? `/adapter-app/` : '/'
   const remotesfn = (scopeName, {port, projectName}) => {
-    const host = isDeploy ? `http://172.29.96.250:3000/${projectName}` : `http://${ip}:${port}`
+    const host = isDeploy ? `https://emp-share.empjs.dev/${projectName}` : `http://${ip}:${port}`
     return `${scopeName}@${host}/emp.json`
   }
   //
