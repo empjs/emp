a0418d36aa66a6fab5be77f0192b6aad576c565d
xuhongbin <xuhongbin@yy.com>
2025-10-11 16:28:32 +0800

    refactor(css): 重构样式文件结构并优化导入方式
    将style.css拆分为多个模块化css文件，包括utilities.css、preflight.css和theme.css
    调整css文件路径引用，更新tsconfig配置以支持jsx
    优化vscode设置中的css验证配置

--- projects/react-19-tanstack/src/index.tsx
+++ projects/react-19-tanstack/src/index.tsx
@@ -1,4 +1,4 @@
-import 'src/style.css'
+import 'src/css/style.css'
 import {createRouter, RouterProvider} from '@tanstack/react-router'
 import {createRoot} from 'react-dom/client'
 
--- projects/react-19-tanstack/src/routes/__root.tsx
+++ projects/react-19-tanstack/src/routes/__root.tsx
@@ -1,20 +1,24 @@
 import {createRootRoute, Link, Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 
-const RootLayout = () => (
-  <div className="react19tt">
-    <div className="p-2 flex gap-2">
-      <Link to="/" className="[&.active]:font-bold">
-        Home
-      </Link>{' '}
-      <Link to="/about" className="[&.active]:font-bold">
-        About
-      </Link>
-    </div>
-    <hr />
-    <Outlet />
-    <TanStackRouterDevtools />
+const Nav = () => (
+  <div className="p-2 flex gap-2 border-b border-gray-300">
+    <Link to="/" className="[&.active]:font-bold">
+      Home
+    </Link>
+    <Link to="/about" className="[&.active]:font-bold">
+      About
+    </Link>
   </div>
+)
+const RootLayout = () => (
+  <>
+    <div className="react19tt">
+      <Nav />
+      <Outlet />
+    </div>
+    <TanStackRouterDevtools />
+  </>
 )
 
 export const Route = createRootRoute({component: RootLayout})
