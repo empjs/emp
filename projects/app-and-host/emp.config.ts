f6b9c95dbbb7a774861da3fe43f01547e8afc64a
xuhongbin <xuhongbin@yy.com>
2025-11-14 15:55:19 +0800

    feat: 启用 dts 生成类型定义

--- projects/app-and-host/emp.config.ts
+++ projects/app-and-host/emp.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(store => {
           setExternals: externalReact,
         },
         manifest: true,
+        dts: true,
         // dts: {
         //   generateTypes: {
         //     generateAPITypes: true,
