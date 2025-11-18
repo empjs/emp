8450931e198a36610374965ef2f7a88accc01057
doerme <398921432@qq.com>
2025-11-18 18:14:48 +0800

    feat: tailwindcss plugin add options

--- packages/plugin-tailwindcss/src/index.ts
+++ packages/plugin-tailwindcss/src/index.ts
@@ -1,7 +1,8 @@
 import type {GlobalStore} from '@empjs/cli'
+import autoprefixer from 'autoprefixer'
 import path from 'path'
 import {TailwindcssOptions} from './types.js'
-export default (tailwindcssOptions: TailwindcssOptions = {}) => {
+export default (tailwindcssOptions: TailwindcssOptions = {}, autoprefixerOptions?: autoprefixer.Options, presetEnvFeature?: Record<string, boolean>) => {
   return {
     name: '@empjs/plugin-tailwindcss',
     async rsConfig(store: GlobalStore) {
@@ -39,9 +40,7 @@ export default (tailwindcssOptions: TailwindcssOptions = {}) => {
           {
             browsers: store.empConfig.build.polyfill.browserslist, // 针对安卓 7 的 Chrome
             stage: 1, // 启用较新的 CSS 特性并提供 polyfill
-            features: {
-              'is-pseudo-class': false, // 正确的特性名称
-            },
+            features: presetEnvFeature,
           },
         ])
       }
@@ -60,6 +59,8 @@ export default (tailwindcssOptions: TailwindcssOptions = {}) => {
           },
         ])
       }
+
+      postcssPlugins.push(autoprefixer(autoprefixerOptions))
       // console.log('postcssPlugins', JSON.stringify(postcssPlugins))
       chain.module
         .rule(store.chainName.rule.css)
