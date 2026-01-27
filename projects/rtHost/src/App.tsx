b48f20564f53b9618a1699029082d6c37fd87000
xuhongbin <xuhongbin@bigo.sg>
2026-01-27 23:39:51 +0800

    chore: update dependencies and enhance functionality
    - Bump @rspack/core to version 1.7.4 and update related dependencies
    - Upgrade @module-federation/runtime and @module-federation/sdk to version 0.23.0
    - Modify splitChunks configuration in RspackCommon for improved code splitting
    - Refactor remote component loading in rtHost to utilize registerRemotes and lazy loading
    - Update CLI version to 3.12.0

--- projects/rtHost/src/App.tsx
+++ projects/rtHost/src/App.tsx
@@ -1,28 +1,39 @@
-import {createInstance} from '@empjs/share/sdk'
+import {createInstance, loadRemote, registerRemotes} from '@empjs/share/sdk'
 import {lazy, Suspense, useCallback, useState} from 'react'
 
-const mf = createInstance({
-  name: 'rtHost',
-  remotes: [
-    {
-      name: 'rtProvider',
-      entry: 'http://localhost:4001/emp.json',
-    },
-    {
-      name: 'rtLayout',
-      entry: 'http://127.0.0.1:4004/emp.json',
-    },
-  ],
-})
-const ProviderApp = lazy(() => mf.loadRemote<any>('rtProvider/App'))
+// const mf = createInstance({
+//   name: 'rtHost',
+//   remotes: [
+//     {
+//       name: 'rtProvider',
+//       entry: 'http://localhost:4001/emp.json',
+//     },
+//     {
+//       name: 'rtLayout',
+//       entry: 'http://127.0.0.1:4004/emp.json',
+//     },
+//   ],
+// })
+// const ProviderApp = lazy(() => mf.loadRemote<any>('rtProvider/App'))
 
 const App = () => {
-  const [instanceIds, setInstanceIds] = useState<number[]>(() => [Date.now()])
+  const [Components, setComponents] = useState<React.ReactNode[]>([])
 
-  const handleAddRemote = useCallback(async () => {
-    await mf.loadRemote<any>('rtProvider/App')
-    setInstanceIds(ids => [...ids, Date.now()])
-  }, [])
+  const handleAddRemote = useCallback(() => {
+    // Use React.lazy to wrap dynamic import for Suspense
+    registerRemotes([
+      {
+        name: 'rtProvider',
+        entry: 'http://localhost:4001/emp.json',
+      },
+      // {
+      //   name: 'rtLayout',
+      //   entry: 'http://127.0.0.1:4004/emp.json',
+      // },
+    ])
+    const RemoteComponent = lazy(() => loadRemote('rtProvider/App'))
+    setComponents(prev => [...prev, <RemoteComponent key={prev.length} />])
+  }, [setComponents])
 
   return (
     <div>
@@ -30,9 +41,9 @@ const App = () => {
       <button type="button" onClick={handleAddRemote}>
         添加远程组件
       </button>
-      {instanceIds.map(id => (
-        <Suspense key={id} fallback={<div>Loading...</div>}>
-          <ProviderApp name={'From Host'} debug={true} status={'Running'} />
+      {Components.map((component, index) => (
+        <Suspense key={index} fallback={<div>Loading...</div>}>
+          {component}
         </Suspense>
       ))}
     </div>
